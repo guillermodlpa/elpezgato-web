ae9f0d9f348b5bb63f28416a9552fdeadbe1a36c
Guillermo de la Puente <g.puente.allott@gmail.com>
2023-09-13 11:47:05 +0200

    Preload mascot image

--- src/components/Mascot.tsx
+++ src/components/Mascot.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Transition } from "@headlessui/react";
+import Head from "next/head";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
@@ -32,27 +33,33 @@ export default function Mascot() {
   }, []);
 
   return (
-    <Transition
-      appear
-      show={visible}
-      enter="duration-500"
-      enterFrom="translate-x-full"
-      enterTo="translate-x-0"
-      leave="duration-500"
-      leaveFrom="translate-x-0"
-      leaveTo="translate-x-full"
-      className={`fixed bottom-0 right-0 z-drawer transition-transform`}
-      role="dialog"
-    >
-      <Image
-        src="/pez_gato_mascota.png"
-        alt="Mascota"
-        className="w-[75px] md:w-[100px]"
-        width={100}
-        height={100}
-        onClick={() => setVisible(false)}
-        onMouseEnter={() => setVisible(false)}
-      />
-    </Transition>
+    <>
+      <Head>
+        <link rel="preload" href="/pez_gato_mascota.png" as="image" />
+      </Head>
+
+      <Transition
+        appear
+        show={visible}
+        enter="duration-500"
+        enterFrom="translate-x-full"
+        enterTo="translate-x-0"
+        leave="duration-500"
+        leaveFrom="translate-x-0"
+        leaveTo="translate-x-full"
+        className={`fixed bottom-0 right-0 z-drawer transition-transform`}
+        role="dialog"
+      >
+        <Image
+          src="/pez_gato_mascota.png"
+          alt="Mascota"
+          className="w-[75px] md:w-[100px]"
+          width={100}
+          height={100}
+          onClick={() => setVisible(false)}
+          onMouseEnter={() => setVisible(false)}
+        />
+      </Transition>
+    </>
   );
 }
